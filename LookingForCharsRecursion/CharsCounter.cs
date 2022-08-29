08861d45fa4960f9f4182633e3edc41addef21f3
Mouz24 <90099528+Mouz24@users.noreply.github.com>
2022-08-29 15:33:52 +0300

    Done

---
+++ LookingForCharsRecursion/CharsCounter.cs
@@ -0,0 +1,143 @@
+﻿using System;
+
+#pragma warning disable CA1062
+
+namespace LookingForCharsRecursion
+{
+    public static class CharsCounter
+    {
+        /// <summary>
+        /// Searches a string for all characters that are in <see cref="Array" />, and returns the number of occurrences of all characters.
+        /// </summary>
+        /// <param name="str">String to search.</param>
+        /// <param name="chars">One-dimensional, zero-based <see cref="Array"/> that contains characters to search for.</param>
+        /// <returns>The number of occurrences of all characters.</returns>
+        public static int GetCharsCount(string str, char[] chars)
+        {
+            if (str is null)
+            {
+                throw new ArgumentNullException(nameof(str));
+            }
+
+            if (chars is null)
+            {
+                throw new ArgumentNullException(nameof(chars));
+            }
+
+            if (str.Length == 0 && chars.Length == 0)
+            {
+                return 0;
+            }
+
+            return LookChars(str, chars, 0, 0, str.Length);
+        }
+
+        /// <summary>
+        /// Searches a string for all characters that are in <see cref="Array" />, and returns the number of occurrences of all characters within the range of elements in the <see cref="string"/> that starts at the specified index and ends at the specified index.
+        /// </summary>
+        /// <param name="str">String to search.</param>
+        /// <param name="chars">One-dimensional, zero-based <see cref="Array"/> that contains characters to search for.</param>
+        /// <param name="startIndex">A zero-based starting index of the search.</param>
+        /// <param name="endIndex">A zero-based ending index of the search.</param>
+        /// <returns>The number of occurrences of all characters within the specified range of elements in the <see cref="string"/>.</returns>
+        public static int GetCharsCount(string str, char[] chars, int startIndex, int endIndex)
+        {
+            if (str is null)
+            {
+                throw new ArgumentNullException(nameof(str));
+            }
+
+            if (chars is null)
+            {
+                throw new ArgumentNullException(nameof(chars));
+            }
+
+            if (startIndex < 0 || startIndex > str.Length || startIndex > endIndex)
+            {
+                throw new ArgumentOutOfRangeException(nameof(startIndex));
+            }
+
+            if (endIndex < 0 || endIndex > str.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(endIndex));
+            }
+
+            if (str.Length == 0 || chars.Length == 0)
+            {
+                return 0;
+            }
+
+            return LookChars(str, chars, startIndex, 0, endIndex + 1);
+        }
+
+        /// <summary>
+        /// Searches a string for a limited number of characters that are in <see cref="Array" />, and returns the number of occurrences of all characters within the range of elements in the <see cref="string"/> that starts at the specified index and ends at the specified index.
+        /// </summary>
+        /// <param name="str">String to search.</param>
+        /// <param name="chars">One-dimensional, zero-based <see cref="Array"/> that contains characters to search for.</param>
+        /// <param name="startIndex">A zero-based starting index of the search.</param>
+        /// <param name="endIndex">A zero-based ending index of the search.</param>
+        /// <param name="limit">A maximum number of characters to search.</param>
+        /// <returns>The limited number of occurrences of characters to search for within the specified range of elements in the <see cref="string"/>.</returns>
+        public static int GetCharsCount(string str, char[] chars, int startIndex, int endIndex, int limit)
+        {
+            if (str is null)
+            {
+                throw new ArgumentNullException(nameof(str));
+            }
+
+            if (chars is null)
+            {
+                throw new ArgumentNullException(nameof(chars));
+            }
+
+            if (startIndex < 0 || startIndex > str.Length || startIndex > endIndex)
+            {
+                throw new ArgumentOutOfRangeException(nameof(startIndex));
+            }
+
+            if (endIndex < 0 || endIndex > str.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(endIndex));
+            }
+
+            if (limit < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(limit));
+            }
+
+            if (str.Length == 0 || chars.Length == 0)
+            {
+                return 0;
+            }
+
+            int count = LookChars(str, chars, startIndex, 0, endIndex + 1);
+            if (count >= limit)
+            {
+                return limit;
+            }
+
+            return count;
+        }
+
+        public static int LookChars(string str, char[] chars, int i, int j, int length, int count = 0)
+        {
+            if (str[i] == chars[j])
+            {
+                count++;
+            }
+
+            if (j < chars.Length - 1)
+            {
+                return LookChars(str, chars, i, j + 1, length, count);
+            }
+
+            if (i == length - 1 && j == chars.Length - 1)
+            {
+                return count;
+            }
+
+            return LookChars(str, chars, i + 1, 0, length, count);
+        }
+    }
+}
